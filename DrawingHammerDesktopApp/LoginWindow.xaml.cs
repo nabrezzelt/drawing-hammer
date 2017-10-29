2025bd7315404573cfdf4c38dd9aa8c1cf6f077d
Nabrezzelt <Nabrezzelt@web.de>
2017-10-30 00:05:11 +0100

    Login and register window created

---
+++ DrawingHammerDesktopApp/LoginWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+
+namespace DrawingHammerDesktopApp
+{
+    /// <summary>
+    /// Interaktionslogik für LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowRegisterWindow(object sender, RoutedEventArgs e)
+        {
+            var registerWindow = new RegisterWindow();
+            registerWindow.ShowDialog();
+        }
+
+        private void Login(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Logging in...");
+        }
+
+        private void CheckForEnablingLoginButton(object sender, RoutedEventArgs routedEventArgs)
+        {
+            if (tbPassword.Password != "" && tbUsername.Text != "")
+            {
+                btnLogin.IsEnabled = true;
+            }
+            else
+            {
+                btnLogin.IsEnabled = false;
+            }
+        }
+    }
+}
---
+++ DrawingHammerDesktopApp/RegisterWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace DrawingHammerDesktopApp
+{
+    /// <summary>
+    /// Interaktionslogik für RegisterWindow.xaml
+    /// </summary>
+    public partial class RegisterWindow : Window
+    {
+        public RegisterWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckForEnableRegisterButton(object sender, RoutedEventArgs routedEventArgs)
+        {
+            if (tbPassword.Password != "" && tbPassword.Password == tbPasswordRepeat.Password && tbUsername.Text != "")
+            {
+                btnRegister.IsEnabled = true;
+            }
+            else
+            {
+                btnRegister.IsEnabled = false;
+            }
+        }
+
+        private void Register(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Register...");
+        }
+    }
+}
