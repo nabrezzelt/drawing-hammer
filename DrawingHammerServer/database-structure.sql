94db2ffd6d89b449afdfbde3995870ddbc855c45
St3lx <florian.vees@outlook.de>
2017-12-10 11:07:14 +0100

    Word-Database und Manager implementiert.

--- DrawingHammerServer/database-structure.sql
+++ DrawingHammerServer/database-structure.sql
@@ -25,6 +25,76 @@ CREATE TABLE IF NOT EXISTS `user_salt` (
   CONSTRAINT `FK_user_salt_users` FOREIGN KEY (`userID`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+DROP TABLE IF EXISTS `words`;
+CREATE TABLE IF NOT EXISTS `words` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `word` varchar(50) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40000 ALTER TABLE `words` DISABLE KEYS */;
+INSERT INTO `words` (`ID`, `word`) VALUES
+	(1, 'Kaffeemaschine'),
+	(2, 'Wasserwaage'),
+	(3, 'Rucksack'),
+	(4, 'Messerblock'),
+	(5, 'Spülmaschine'),
+	(6, 'Tiefgarage'),
+	(7, 'Helikopter'),
+	(8, 'Fußballstadion'),
+	(9, 'Handy'),
+	(10, 'Tastatur'),
+	(11, 'Sonnenuntergang'),
+	(12, 'Jahreszeiten'),
+	(13, 'Nacht'),
+	(14, 'Rollkragenpullover'),
+	(15, 'Unterwäsche'),
+	(16, 'Bus'),
+	(17, 'Bahnhof'),
+	(18, 'Autobahnausfahrt'),
+	(19, 'Polizeikontrolle'),
+	(20, 'Datenbank'),
+	(21, 'Decke'),
+	(22, 'Sattelitenschüssel'),
+	(23, 'Salatschleuder'),
+	(24, 'Kalender'),
+	(25, 'Auflaufform'),
+	(26, 'Serverschrank'),
+	(27, 'Fernbedienung'),
+	(28, 'Perrücke'),
+	(29, 'Beerdigung'),
+	(30, 'Geldbeutel'),
+	(31, 'Monitor'),
+	(32, 'Schreibtischstuhl'),
+	(33, 'Einkaufswagen'),
+	(34, 'Verkehrspolizist'),
+	(35, 'Fahrschule'),
+	(36, 'Prüfung'),
+	(38, 'Arbeitsmoral'),
+	(39, 'Schokonikolaus'),
+	(40, 'Schlitten'),
+	(41, 'Badehose'),
+	(42, 'Muskelkater'),
+	(43, 'Fitnesstudio'),
+	(44, 'Abschlussarbeit'),
+	(45, 'Wartezimmer'),
+	(46, 'Zahnarztpraxis'),
+	(47, 'Marathonläufer'),
+	(49, 'Urlaub'),
+	(50, 'Flugzeug');
+/*!40000 ALTER TABLE `words` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+
+
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
