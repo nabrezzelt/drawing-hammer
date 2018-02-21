8b78ec765c0b4a736ee40e63365292cb837d8997
St3lx <florian.vees@outlook.de>
2018-02-21 10:23:11 +0100

    Updated database-structure

--- DrawingHammerServer/database-structure.sql
+++ DrawingHammerServer/database-structure.sql
@@ -25,6 +25,32 @@ CREATE TABLE IF NOT EXISTS `user_salt` (
   CONSTRAINT `FK_user_salt_users` FOREIGN KEY (`userID`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id`, `username`, `password`, `isBanned`) VALUES
+	(1, '123456', '2AAA9630D41B3E3050EDBD899669BA48A94AB86E47ECD22CFDD5A6F868AB720D', 0),
+	(2, '654321', '55D9D475B997A77DB1BEF96458BF4218AA9070A05EBC2DC167AFF13C07E21027', 0),
+	(3, 'qwertz', 'D4228FBB276695171685771570B199221BE0F2AAB22D8F1FB94AC8D478CF7DDE', 0),
+	(4, 'asdfgh', '3DE2BFF1C461478EDFF6495268C07B075989E8CA7C7167013F9C30949197E9A6', 0);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
+CREATE TABLE IF NOT EXISTS `user_salt` (
+  `userID` int(11) NOT NULL,
+  `salt` varchar(250) NOT NULL,
+  PRIMARY KEY (`userID`),
+  CONSTRAINT `FK_user_salt_users` FOREIGN KEY (`userID`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Exportiere Daten aus Tabelle drawing-hammer.user_salt: ~4 rows (ungefähr)
+/*!40000 ALTER TABLE `user_salt` DISABLE KEYS */;
+INSERT INTO `user_salt` (`userID`, `salt`) VALUES
+	(1, 'VSs6Tc2hy7nceAVFiPXbQO3QA0Ox5rebGXm5PXqhUqp1eesYuUiLKsIbg6Ik0'),
+	(2, 'EU9HQHTUf0kpAqRd9p3tJg3CE7GkgYIszWOMgyAhSEjfdwITfGmhRvnVUPUMXziE'),
+	(3, 'saBw3S4GWXGXxoJm3Zq4gwSVnmnD7dfgttBBLy4bJU5E1SnNvUXSoFtnvGy50zcDU'),
+	(4, 'cln1EiqkL0XUYK7U5k1nwcD4E4SvOby3TQldc1DZIyeAz6vViF1xoqKKH1ihWjSQ');
+/*!40000 ALTER TABLE `user_salt` ENABLE KEYS */;
+
+
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
@@ -39,55 +65,55 @@ CREATE TABLE IF NOT EXISTS `words` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*!40000 ALTER TABLE `words` DISABLE KEYS */;
-INSERT INTO `words` (`ID`, `word`) VALUES
-	(1, 'Kaffeemaschine'),
-	(2, 'Wasserwaage'),
-	(3, 'Rucksack'),
-	(4, 'Messerblock'),
-	(5, 'Spülmaschine'),
-	(6, 'Tiefgarage'),
-	(7, 'Helikopter'),
-	(8, 'Fußballstadion'),
-	(9, 'Handy'),
-	(10, 'Tastatur'),
-	(11, 'Sonnenuntergang'),
-	(12, 'Jahreszeiten'),
-	(13, 'Nacht'),
-	(14, 'Rollkragenpullover'),
-	(15, 'Unterwäsche'),
-	(16, 'Bus'),
-	(17, 'Bahnhof'),
-	(18, 'Autobahnausfahrt'),
-	(19, 'Polizeikontrolle'),
-	(20, 'Datenbank'),
-	(21, 'Decke'),
-	(22, 'Sattelitenschüssel'),
-	(23, 'Salatschleuder'),
-	(24, 'Kalender'),
-	(25, 'Auflaufform'),
-	(26, 'Serverschrank'),
-	(27, 'Fernbedienung'),
-	(28, 'Perrücke'),
-	(29, 'Beerdigung'),
-	(30, 'Geldbeutel'),
-	(31, 'Monitor'),
-	(32, 'Schreibtischstuhl'),
-	(33, 'Einkaufswagen'),
-	(34, 'Verkehrspolizist'),
-	(35, 'Fahrschule'),
-	(36, 'Prüfung'),
-	(38, 'Arbeitsmoral'),
-	(39, 'Schokonikolaus'),
-	(40, 'Schlitten'),
-	(41, 'Badehose'),
-	(42, 'Muskelkater'),
-	(43, 'Fitnesstudio'),
-	(44, 'Abschlussarbeit'),
-	(45, 'Wartezimmer'),
-	(46, 'Zahnarztpraxis'),
-	(47, 'Marathonläufer'),
-	(49, 'Urlaub'),
-	(50, 'Flugzeug');
+INSERT INTO `words` (`word`) VALUES
+	('Kaffeemaschine'),
+	('Wasserwaage'),
+	('Rucksack'),
+	('Messerblock'),
+	('Spülmaschine'),
+	('Tiefgarage'),
+	('Helikopter'),
+	('Fußballstadion'),
+	('Handy'),
+	('Tastatur'),
+	('Sonnenuntergang'),
+	('Jahreszeiten'),
+	('Nacht'),
+	('Rollkragenpullover'),
+	('Unterwäsche'),
+	('Bus'),
+	('Bahnhof'),
+	('Autobahnausfahrt'),
+	('Polizeikontrolle'),
+	('Datenbank'),
+	('Decke'),
+	('Sattelitenschüssel'),
+	('Salatschleuder'),
+	('Kalender'),
+	('Auflaufform'),
+	('Serverschrank'),
+	('Fernbedienung'),
+	('Perrücke'),
+	('Beerdigung'),
+	('Geldbeutel'),
+	('Monitor'),
+	('Schreibtischstuhl'),
+	('Einkaufswagen'),
+	('Verkehrspolizist'),
+	('Fahrschule'),
+	('Prüfung'),
+	('Arbeitsmoral'),
+	('Schokonikolaus'),
+	('Schlitten'),
+	('Badehose'),
+	('Muskelkater'),
+	('Fitnesstudio'),
+	('Abschlussarbeit'),
+	('Wartezimmer'),
+	('Zahnarztpraxis'),
+	('Marathonläufer'),
+	('Urlaub'),
+	('Flugzeug');
 /*!40000 ALTER TABLE `words` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
